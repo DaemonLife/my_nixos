1c3f93f7962f347384539e5465a0b3399894c7d0
DaemonLife <beleevskij21@gmail.com>
2026-02-20 14:51:42 +0300

    add mini plugins to nixvim remove other

--- modules/editor/nixvim.nix
+++ modules/editor/nixvim.nix
@@ -41,23 +41,37 @@
           python
         ];
       };
-      telescope.enable = true;
       nix.enable = true;
       render-markdown.enable = true;
       colorizer.enable = true; # colors for hex code
-      comment.enable = true;
-      nvim-autopairs.enable = true; # auto ""
-      nvim-surround.enable = true; # auto "[text]"
+
+      mini = {
+        enable = true;
+        mockDevIcons = true;
+        modules = {
+          basics = { };
+          comment = { };
+          completion = { };
+          surround = { };
+          pairs = { };
+          animate = { };
+          icons = { };
+          notify = { };
+          starter = { }; # start screen
+          statusline = { };
+          git = { };
+          diff = { };
+          pick = { }; # like telescope
+        };
+      };
 
       lsp = {
         enable = true;
-
         servers = {
           bashls.enable = true;
           html.enable = true;
-          # nix language server
           nil_ls = {
-            enable = true;
+            enable = true; # nix language server
             settings = {
               nix.flake.autoArchive = true;
               formatting.command = [ "nixpkgs-fmt" ]; # autoformat
@@ -65,30 +79,6 @@
           };
         };
 
-      };
-
-      # autocomplite
-      cmp = {
-        enable = true;
-        autoEnableSources = true;
-        settings = {
-          sources = [
-            { name = "nvim_lsp"; }
-            { name = "path"; }
-            { name = "buffer"; }
-            { name = "luasnip"; }
-          ];
-          mapping = {
-            "<C-Space>" = "cmp.mapping.complete()";
-            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-e>" = "cmp.mapping.close()";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<CR>" = "cmp.mapping.confirm({ select = true })";
-            "<S-Tab>" =
-              "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          };
-        };
       };
 
       lsp-format = {
@@ -102,18 +92,7 @@
         settings = { indent.char = "│"; };
       };
 
-      lightline = {
-        enable = true;
-        settings = {
-          active = {
-            left = [ [ "mode" "paste" ] [ "readonly" "filename" "modified" ] ];
-            right = [ [ "lineinfo" ] [ "percent" ] [ "fileformat" "fileencoding" "filetype" ] ];
-          };
-        };
-      };
-
     };
-    # --- Plugins ---
 
     opts = {
       # Line numbers
@@ -133,21 +112,15 @@
       smarttab = true;
       autoindent = true; # Do clever autoindenting
 
-      # Highlight the current line
-      cursorline = true;
-
-      # Show line and column when searching
-      ruler = true;
-
-      # Start scrolling when the cursor is X lines away from the top/bottom
-      scrolloff = 4;
-
       # Other settings
+      cursorline = true; # Highlight the current line
+      ruler = true; # Show line and column when searching
       swapfile = false; # Disable the swap file
       fileencoding = "utf-8"; # File-content encoding for the current buffer
       spell = true; # Highlight spelling mistakes (local to window)
       wrap = true;
       linebreak = true;
+      scrolloff = 4;
       termguicolors = true; # like base16 color scheme for me
     };
 
@@ -166,25 +139,11 @@
       { event = "FileType"; pattern = "nix"; command = "setlocal tabstop=2 shiftwidth=2"; }
     ];
 
-    highlightOverride = {
-      # status bar 
-      "LightlineLeft_active_0" = { bg = "#${base0D}"; fg = "#${base00}"; };
-      "LightlineLeft_active_1" = { bg = "#${base01}"; fg = "#${base03}"; };
-      "LightlineMiddle_active" = { bg = "#${base01}"; fg = "#${base03}"; };
-      "LightlineRight_active_0" = { bg = "#${base01}"; fg = "#${base03}"; };
-      "LightlineRight_active_1" = { bg = "#${base01}"; fg = "#${base03}"; };
-      "LightlineRight_active_2" = { bg = "#${base01}"; fg = "#${base03}"; };
-      # number bar
-      "LineNrAbove" = { bg = "#${base00}"; fg = "#${base03}"; };
-      "CursorLineNr" = { bg = "#${base00}"; fg = "#${base05}"; };
-      "LineNrBelow" = { bg = "#${base00}"; fg = "#${base03}"; };
-    };
-
     keymaps = [
       { action = "<leader>"; key = " "; } # space is leader
       {
         action = ""; # don't make an annoying space 
-        key = "<leader>"; 
+        key = "<leader>";
         mode = [ "v" ];
       }
       {
@@ -205,26 +164,38 @@
       {
         mode = "n";
         key = "<leader>ff";
-        action = "<cmd>Telescope find_files<CR>";
+        action = "<cmd>Pick files<CR>";
         options.desc = "Find files";
       }
       {
         mode = "n";
         key = "<leader>fg";
-        action = "<cmd>Telescope live_grep<CR>";
+        action = "<cmd>Pick grep_live<CR>";
         options.desc = "Live grep";
       }
       {
         mode = "n";
+        key = "<leader>fb";
+        action = "<cmd>Pick buffers<CR>";
+        options.desc = "Live buffers";
+      }
+      {
+        mode = "n";
         key = "<leader>аа"; # rus
-        action = "<cmd>Telescope find_files<CR>";
+        action = "<cmd>Pick files<CR>";
         options.desc = "Find files";
       }
       {
         mode = "n";
         key = "<leader>ап"; # rus
-        action = "<cmd>Telescope live_grep<CR>";
+        action = "<cmd>Pick grep_live<CR>";
         options.desc = "Live grep";
+      }
+      {
+        mode = "n";
+        key = "<leader>аи";
+        action = "<cmd>Pick buffers<CR>";
+        options.desc = "Live buffers";
       }
 
       # --- new redo ---
