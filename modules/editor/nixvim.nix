da9434b4243e2954d0398c09e2231e5f95588475
DaemonLife <beleevskij21@gmail.com>
2026-02-28 21:40:50 +0300

    upd nixvim

--- modules/editor/nixvim.nix
+++ modules/editor/nixvim.nix
@@ -37,78 +37,28 @@
       nix.enable = true;
       render-markdown.enable = true;
       colorizer.enable = true; # colors for hex code
-
-      indent-blankline = {
-        enable = true;
-        settings = { indent.char = "│"; };
+      indent-blankline = { enable = true; settings.indent.char = "│"; };
+      lualine = {
+        enable = true; # statusline
+        settings.options = {
+          icons_enabled = false;
+          section_separators = "";
+          component_separators = "";
+        };
       };
 
       mini = {
         enable = true;
-        # mockDevIcons = true;
         modules = {
           basics = { };
           comment = { };
           completion = { };
           surround = { };
           pairs = { };
-          # animate = { };
-          # icons = { };
           notify = { };
-          # starter = { }; # start screen
           git = { };
           diff = { };
           pick = { }; # like telescope
-          statusline = {
-            enable = true;
-            settings = {
-              use_icons = false;
-            };
-            content.active.__raw = '' 
-              function()
-                local MiniStatusline = require('mini.statusline')
-
-                local filetype = function()
-                  local ft = vim.bo.filetype
-                  return string.format('%s', ft)
-                end
-
-                local location = function(args)
-                  if MiniStatusline.is_truncated(args.trunc_width) then
-                    return '%02l|%02v'
-                  end
-                  return '%02l/%02L|%02v/%02{virtcol("$")-1}'
-                end
-
-                -- Use only HEAD name as summary string for GIT
-                local format_summary = function(data)
-                  local summary = vim.b[data.buf].minigit_summary
-                  vim.b[data.buf].minigit_summary_string = summary.head_name or ""
-                end
-                local au_opts = { pattern = "MiniGitUpdated", callback = format_summary }
-                vim.api.nvim_create_autocmd("User", au_opts)
-
-                local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120, icon = "" })
-                local git           = MiniStatusline.section_git({ trunc_width = 40, icon = "" })
-                local diff          = MiniStatusline.section_diff({ trunc_width = 75, icon = "" })
-                local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75, icon = "",
-                                      signs = {ERROR = 'x', WARN = '!', INFO = '?', HINT = '*'} })
-                local filename      = MiniStatusline.section_filename({ trunc_width = 140, icon = "" })
-                local location      = location({ trunc_width = 75, icon = "" })
-                local search        = MiniStatusline.section_searchcount({ trunc_width = 75, icon = "" })
-                
-                return MiniStatusline.combine_groups({
-                  { hl = mode_hl,                  strings = {mode:upper()}},
-                  { hl = 'MiniStatuslineDevinfo',  strings = {git, diff}},
-                  '%<', -- Mark general truncate point
-                  { hl = 'MiniStatuslineFilename', strings = { filename } },
-                  '%=', -- End left alignment
-                  { hl = 'MiniStatuslineDevinfo',  strings = { filetype(), diagnostics } },
-                  { hl = mode_hl,                  strings = { search, location } },
-                })
-              end
-            '';
-          };
         };
       };
 
@@ -126,8 +76,8 @@
           };
         };
       };
-
       lsp-format = { enable = true; lspServersToEnable = "all"; };
+
     };
 
     opts = {
@@ -160,14 +110,14 @@
       termguicolors = true; # like base16 color scheme for me
     };
 
-    extraConfigVim = "
+    extraConfigVim = ''
         \" enable title and setup
         set title
         set titlestring=nvim
 
         \" transparent bg
         autocmd VimEnter * highlight Normal guibg=NONE ctermbg=NONE
-      ";
+      '';
 
     # Setups for some files 
     autoCmd = [
@@ -196,7 +146,7 @@
         options.remap = true;
       }
 
-      # telescope
+      # mini search (like telescope)
       {
         mode = "n";
         key = "<leader>ff";
