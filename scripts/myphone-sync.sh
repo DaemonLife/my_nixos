f2543252a0fd09dda27847a236e4880ea34daf7d
DaemonLife <beleevskij21@gmail.com>
2026-03-21 03:46:23 +0300

    change sync script

--- scripts/myphone-sync.sh
+++ scripts/myphone-sync.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Important! Check date format for jrnl on both devices
+
 myphone_username=$1
 myphone_port=$2
 PHONE_HOME="/data/data/com.termux/files/home"
@@ -24,6 +26,6 @@ echo
 
 scp -P $myphone_port $myphone_username@myphone:$PHONE_HOME/.local/share/jrnl/journal.txt /tmp/ \
 && jrnl --import --file /tmp/journal.txt \
-&& echo && echo -e "Creating a backup and clearing current journal \n(for secure synchronization later) on your phone." \
+&& echo && echo -e "Creating a backup and clearing current journal \n  (for secure synchronization later) on your phone." \
 && ssh -p $myphone_port $myphone_username@myphone "cd $PHONE_HOME && cd .local/share/jrnl && touch journal.txt_bkp && jrnl --import --file ./journal.txt_bkp 2> /dev/null && mv journal.txt journal.txt_bkp -f && touch journal.txt && echo -e '. \n. \n. . . Done'" \
-&& echo -e "\nSync complited!"
+&& echo -e "\nSync complete!"
